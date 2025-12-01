b13902e061daffb6d96ea32db1e49db1dff6f1cb
SabbirHossainEvan <evansabbir31@gmail.com>
2025-12-01 19:36:36 +0600

    tab section

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -3,6 +3,7 @@ import { Tabs } from 'expo-router'
 import React from 'react'
 import { useColorScheme } from 'react-native'
 import { Colors } from '../../constants/Colors'
+import { Ionicons } from '@expo/vector-icons'
 
 const DashboardLayout = () => {
     const colorScheme = useColorScheme()
@@ -11,7 +12,9 @@ const DashboardLayout = () => {
     <Tabs
         screenOptions={{headerShown: false, tabBarStyle: { backgroundColor: theme.navBackground, paddingTop: 10, height: 90 }, tabBarActiveTintColor: theme.iconColorFocused, tabBarInactiveTintColor: theme.iconColor }}
     >
-        <Tabs.Screen name="profile" options={{ title: 'Profile' }} />
+        <Tabs.Screen name="profile" options={{ title: 'Profile', tabBarIcon: () => (
+            <Ionicons size={24} name='person' />
+        ) }} />
         <Tabs.Screen name="books" options={{ title: 'Books' }} />
         <Tabs.Screen name="create" options={{ title: 'Create' }} />
     </Tabs>
